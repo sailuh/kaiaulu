3cb1fe2cc87c55d3d86823588f8bf6a110320072
Carlos Paradis <carlosviansi@gmail.com>
2020-05-23 16:47:52 -1000

    i #4 Add communication network parser and viz
    The vignette was renamed to showcase various networks.
    
    The mbox_downloader.sh was provided by Wolfgang. In a future
    issue, this should be integrated into R code for full automation.
    
    R/parsers.R have 2 new functions to use Perceval to parse mbox,
    and to use the parsed mbox into an igraph object were added.
    The vignette  uses the igraph object to create a
    interactive visualization of the data.

--- R/parsers.R
+++ R/parsers.R
@@ -30,6 +30,28 @@ parse_gitlog <- function(perceval_path,git_repo_path){
                              stdout = TRUE,
                              stderr = FALSE)
   # Return the parsed JSON output.
-  return(jsonlite::stream_in(textConnection(perceval_output)))
-  #return(jsonlite::read_json(perceval_output))
+  return(data.table(jsonlite::stream_in(textConnection(perceval_output))))
+}
+parse_mbox <- function(perceval_path,mbox_path){
+  # Remove ".mbox"
+  mbox_uri <- str_split(git_repo_path,pattern=".mbox")[[1]][1]
+  # Use percerval to parse mbox_path. --json line is required to be parsed by jsonlite::fromJSON.
+  perceval_output <- system2(perceval_path,
+                             args = c('mbox',mbox_uri,mbox_path,'--json-line'),
+                             stdout = TRUE,
+                             stderr = FALSE)
+  # Return the parsed JSON output as a data.table.
+  return(data.table(jsonlite::stream_in(textConnection(perceval_output))))
+}
+parse_mbox_igraph <- function(project_mbox){
+  # Obtain the relevant columns - Author, E-mail Thread, and Timestamp
+  mbox_edgelist <- project_mbox[,.(author=data.From,thread=data.Subject,date=data.Date)]
+  # Parse igraph from edgelist
+  mbox_network <- igraph::graph_from_data_frame(mbox_edgelist,directed=TRUE)
+  # Color authors black, and e-mail threads lightblue
+  igraph::V(mbox_network)$color <- ifelse(igraph::V(mbox_network)$name %in% mbox_edgelist$author,
+                                          "black",
+                                          "lightblue")
+  # Return the parsed JSON output as an igraph object.
+  return(mbox_network)
 }
