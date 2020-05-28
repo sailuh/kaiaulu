9da2897fe4dc320072fb8fd98b13c0c24430b3e6
Carlos Paradis <carlosviansi@gmail.com>
2020-05-27 16:43:03 -1000

    i #12 Add MPL 2.0 License
    As of this commit, the code only Imports the
    following packages:
    
     * data.table (MPL 2.0)
     * stringi (BSD-3-clause)
     * jsonlite (Apache 2.0)
    
    Moreover, the use of Perceval (GPL 3.0) is done by command line,
    and can be replaced by any other commit log or mailing list log parsers,
    as will be done in future commits. This, to the best of my knowledge,
    as I am not a lawyer, complies with GPL based on:
    
    https://www.gnu.org/licenses/gpl-faq.html#MereAggregation
    
    and other open source packages like XGBoost (i #1338), in respect
    to clarify the  codebase does not depend on a particular GPL project
    to function.
    
    Author roles were also rectified.

--- R/metric.R
+++ R/metric.R
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Conversion to numeric must be performed before function call.
 #' @export
 metric_churn <- function(lines_added,lines_removed){
--- R/parsers.R
+++ R/parsers.R
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #' @export
 parse_gitlog <- function(perceval_path,git_repo_path,save_path=NA){
   # Remove ".git"
