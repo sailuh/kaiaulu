5885e76051284b9102edd7f3185768b17e518f34
Carlos Paradis <carlosviansi@gmail.com>
2020-05-29 03:41:25 -1000

    i #33 Removes gitlog overhead from parser
    After further experimentation of Perceval interface,
    it seems the git command can be executed for local repos.
    The readme example first suggests it was for online repos only.
    Thus, this commit removes the unecessary step of generating a
    git log in order to parse the git repo of interest.

--- R/parsers.R
+++ R/parsers.R
@@ -4,34 +4,9 @@
 
 #' @export
 parse_gitlog <- function(perceval_path,git_repo_path,save_path=NA){
-  # Remove ".git"
-  git_uri <- stri_replace_last(git_repo_path,replacement="",regex=".git")
-  # The log will be saved to the /tmp/ folder
-  gitlog_path <- "/tmp/gitlog.log"
-  # Execute shell command to extract gitlog using Percerval recommended format (See it's README.md.
-  perceval_output <- system2("git",
-                             args = c('--git-dir',
-                                      git_repo_path,
-                                      'log',
-                                      '--raw',
-                                      '--numstat',
-                                      '--pretty=fuller',
-                                      '--decorate=full',
-                                      '--parents',
-                                      '--reverse',
-                                      '--topo-order',
-                                      '-M',
-                                      '-C',
-                                      '-c',
-                                      '--remotes=origin',
-                                      '--all',
-                                      '>' ,
-                                      gitlog_path),
-                             stdout = TRUE,
-                             stderr = FALSE)
-  # Use percerval to parse gitlog_path. --json line is required to be parsed by jsonlite::fromJSON.
+  # Use percerval to parse .git --json line is required to be parsed by jsonlite::fromJSON.
   perceval_output <- system2(perceval_path,
-                             args = c('git', '--git-log',gitlog_path,git_uri,'--json-line'),
+                             args = c('git',git_repo_path,'--json-line'),
                              stdout = TRUE,
                              stderr = FALSE)
   # Parsed JSON output.
