4c4566edb2e5fabb2998a4179b4e4b1aeada631e
Carlos Paradis <carlosviansi@gmail.com>
2020-06-08 04:07:28 -1000

    i #12 Add missing license header
    The entire package was already licensed under MPL 2.0.
    This just adds a missing header.
    
    Signed-off-by: Carlos Paradis <carlosviansi@gmail.com>

--- R/string.R
+++ R/string.R
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #' @export
 normalized_levenshtein <- function(a,b){
   return(drop(1 - adist(a,b)/max(nchar(a),nchar(b))))
