cf7e41ffd3764725e74753d076018991eade57c6
Carlos Paradis <carlosviansi@gmail.com>
2021-03-01 04:56:32 -1000

    Adds Commit Endpoint and Parsers
    This commit adds 3 functions to Github API interface:
    
    An endpoint to make API calls to the committer end point and
    two parser functions to obtain a .csv of the returned jsons.
    
    One for commit endpoint and another for issue events.
    Note you can use any saved json from previous run to the parser
    functions.
    
    Signed-off-by: Carlos Paradis <carlosviansi@gmail.com>

--- R/github.R
+++ R/github.R
@@ -13,6 +13,70 @@ github_api_project_issue <- function(owner,repo,token){
          per_page=100,
          .token=token)
 }
+
+
+# Parser
+#' @export
+github_parse_project_issue <- function(api_responses){
+  parse_response <- function(api_response){
+    parsed_response <- list()
+    parsed_response[["id"]] <- api_response[["id"]]
+    parsed_response[["created_at"]] <- api_response[["created_at"]]
+    parsed_response[["commit_id"]] <- api_response[["commit_id"]]
+    parsed_response[["event"]] <- api_response[["event"]]
+    parsed_response[["actor_login"]] <- api_response[["actor"]][["login"]]
+    parsed_response[["actor_id"]] <- api_response[["actor"]][["id"]]
+    parsed_response[["actor_type"]] <- api_response[["actor"]][["type"]]
+    parsed_response[["issue_number"]] <- api_response[["issue"]][["number"]]
+    parsed_response[["issue_title"]] <- api_response[["issue"]][["title"]]
+    parsed_response[["issue_user_login"]] <- api_response[["issue"]][["user"]][["login"]]
+    parsed_response[["issue_user_id"]] <- api_response[["issue"]][["user"]][["id"]]
+    parsed_response[["issue_user_site_admin"]] <- api_response[["issue"]][["user"]][["site_admin"]]
+    parsed_response[["issue_state"]] <- api_response[["issue"]][["state"]]
+    parsed_response[["issue_author_association"]] <- api_response[["issue"]][["author_association"]]
+    parsed_response[["issue_body"]] <- api_response[["issue"]][["body"]]
+    parsed_response[["issue_assignee_login"]] <- api_response[["issue"]][["assignee"]][["login"]]
+    parsed_response[["issue_assignee_id"]] <- api_response[["issue"]][["assignee"]][["id"]]
+    parsed_response[["issue_body"]] <- api_response[["issue"]][["body"]]
+
+    assignees_list <- api_response[["issue"]][["assignees"]]
+    assignees_list <- lapply(assignees_list,function(x){
+      data.table(data.frame(issue_assignees_login=x[["login"]],issue_assignees_id=x[["id"]]))
+      })
+    assignees_list <- rbindlist(assignees_list,fill=TRUE)
+    parsed_response[["issue_assignees_login"]] <- stringi::stri_c(assignees_list$issue_assignees_login,collapse = ";")
+    parsed_response[["issue_assignees_id"]] <- stringi::stri_c(assignees_list$issue_assignees_id,collapse = ";")
+    return(data.table(data.frame(parsed_response)))
+  }
+  rbindlist(lapply(api_responses,parse_response),fill=TRUE)
+}
+# https://docs.github.com/en/rest/reference/repos#commits
+#' @export
+github_api_project_commits <- function(owner,repo,token){
+  gh::gh("GET /repos/{owner}/{repo}/commits",
+         owner=owner,
+         repo=repo,
+         page=1,
+         per_page=100,
+         .token=token)
+}
+# Parser
+# under commit(author:name and email), committer(name and email), message. Under author (login), under committer (login)
+#' @export
+github_parse_project_commits <- function(api_responses){
+  parse_response <- function(api_response){
+    parsed_response <- list()
+    parsed_response[["author_login"]] <- api_response[["author"]][["login"]]
+    parsed_response[["commit_author_name"]] <- api_response[["commit"]][["author"]][["name"]]
+    parsed_response[["commit_author_email"]] <- api_response[["commit"]][["author"]][["email"]]
+    parsed_response[["committer_login"]] <- api_response[["committer"]][["login"]]
+    parsed_response[["commit_committer_name"]] <- api_response[["commit"]][["committer"]][["name"]]
+    parsed_response[["commit_committer_email"]] <- api_response[["commit"]][["committer"]][["email"]]
+    parsed_response[["commit_message"]] <- api_response[["commit"]][["message"]]
+    return(data.table(data.frame(parsed_response)))
+  }
+  rbindlist(lapply(api_responses,parse_response),fill=TRUE)
+}
 # https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#list-repository-contributors
 #' @export
 github_api_project_contributor <- function(owner,repo,token){
