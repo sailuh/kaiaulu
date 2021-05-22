eb022fb2ffc8d23725dad842e55a05d9a6e59cf5
CorneJB <49902683+CorneJB@users.noreply.github.com>
2021-05-22 23:04:51 +0200

    i #93 add pipermail downloader
    * Add new download.R functionality
    * Add download.R functionality to NEWS.md New Features
    * Add needed imports for download.R
    * Add Corné as ctb in DESCRIPTION
    
    Signed-off-by: Corné Broere<cb@case-electronics.nl>

---
+++ R/download.R
@@ -0,0 +1,103 @@
+#' Download all pipermail files in an archive
+#' @param url An url pointing to a pipermail archive
+#' @return Returns `destination`, a vector of the downloaded files in the current working directory
+#' @export
+download_pipermail <- function(url) {
+
+  #Get page
+  pagedata <- httr::GET(url)
+
+  #Parse html file into object
+  tbls_xml <- XML::htmlParse(pagedata)
+
+  #Exctract href tablenodes from html table
+  tableNodes <- XML::getNodeSet(tbls_xml, "//td/a[@href]")
+
+  #Exctract filenames from tablenode content with xmlGetAtrr
+  hrefs <- sapply(tableNodes, XML::xmlGetAttr, 'href')
+
+  #Create Vector
+  files <- vector()
+
+  #Compose download urls for both gunzipped and plain text files
+  for (i in hrefs ){
+    if (endsWith(i, ".txt.gz")){
+          i <- paste0(url, i)
+          files <- c(files, i)
+    } else if (endsWith(i, ".txt")) {
+          i <- paste0(url, i)
+          files <- c(files, i)
+    }
+  }
+
+  destination <- vector()
+  # File downloading loop
+  for (i in files){
+
+    #split filename from url and create download destination out of it
+    splits <- stringi::stri_split_fixed(i, "/")
+    destination[[i]] <- paste0(splits[[1]][[length(splits[[1]])]])
+
+    #download file and place it at the destination
+    httr::GET(i, httr::write_disk(destination[[i]], overwrite=TRUE))
+  }
+
+  #Return filenames
+  return(destination)
+
+}
+
+
+#' Convert pipermail archive files (.txt and .txt.gz) into an mbox format for use with \code{\link{parse_mbox}}
+#' @param filelist A vector of pipermail archive files from \code{\link{download_pipermail}}
+#' @return Returns `output`, the name of the resulting .mbox file in the current working directory
+#' @export
+convert_pipermail_to_mbox <- function(filelist) {
+
+  #at to @ replace function
+  pipermail_atreplacer <- function(string) {
+
+    rstring <- sub(" at ", "@", string)
+
+    return(rstring)
+
+  }
+
+  output <- "output.mbox"
+
+  #Create mbox file and file connection
+  file.create(output)
+  fileConn <- file(output, "w+")
+
+
+  #Read lines from downloaded files and write them to mbox file
+  for (filename in filelist[]){
+
+    #Open read connection
+    readCon <- file(filename, "r")
+
+    data <- readLines(filename)
+
+    #Find email headers to send to 'at' to @ replacer
+    for (i in 1:length(data)) {
+
+      data[i] <- sub("From:? \\S+ at \\S+", pipermail_atreplacer(data[i]), data[i])
+
+    }
+
+    #Write files to output
+    writeLines(data, fileConn)
+
+    #Close read connection
+    close(readCon)
+
+    #Delete the file
+    unlink(filename, force = TRUE)
+  }
+
+  #Close connection to mbox file
+  close(fileConn)
+
+  #return output location
+  return(output)
+}
