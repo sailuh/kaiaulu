d194c9613da8259baf078cc431198327921674af
malialiu <76604944+malialiu@users.noreply.github.com>
2023-03-30 22:07:12 -1000

    i sailuh#170 add dv8 function

--- R/dv8.R
+++ R/dv8.R
@@ -12,13 +12,13 @@ dv8_dsmj_to_dsmb <- function(sdsm_json_path, dv8_dsm_save_path){
   system2('dv8-console', args=c('core:convert-matrix', '-outputFile', dv8_dsm_save_path, sdsm_json_path), stdout=FALSE, stderr=FALSE)
 }
 
-#' Merge matrices from sdsm.dv8-dsm file to one new matrix in hsdsm.dv8-dsm
+#' Merge matrices from dv8-dsm file to one new matrix in hsdsm.dv8-dsm
 #'
 #' @param dv8_dsm_path_1 path to the matrix from sdsm.dv8-dsm file
 #' @param dv8_dsm_path_2 path to the matrix from sdsm.dv8-dsm file
 #' @param merged_dv8_dsm_path path to save the merged matrices in hsdsm.dv8-dsm file
 #' @export
-dv8_sdsmb_to_hsdsmb <- function(dv8_dsm_path_1, dv8_dsm_path_2, merged_dv8_dsm_path){
+dv8_hdsmb_sdsmb_to_hsdsmb <- function(dv8_dsm_path_1, dv8_dsm_path_2, merged_dv8_dsm_path){
   # Expand paths (e.g. "~/Desktop" => "/Users/someuser/Desktop")
   dv8_dsm_path_1 <- path.expand(dv8_dsm_path_1)
   dv8_dsm_path_2 <- path.expand(dv8_dsm_path_2)
@@ -40,4 +40,27 @@ dv8_clsxj_to_clsxb <- function(input_path, output_path){
 
   # Run system2 command
   system2('dv8-console', args=c('core:convert-cluster', '-outputFile', output_path, input_path), stdout=FALSE, stderr=FALSE)
+}
+
+#' Detect Architecture anti-patterns
+#'
+#' @param input_path path to the merged matrix input file dv8-dsm
+#' @param output_path path to architecture folder
+#' @export
+dv8_hsdsmb_to_flaws <- function(input_path, output_path){
+  # Expand paths (e.g. "~/Desktop" => "/Users/someuser/Desktop")
+  input_path <- path.expand(input_path)
+  output_path <- path.expand(output_path)
+
+  # Run system2 command with appropriate values for options
+  system2('dv8-console', args=c('arch-issue:arch-issue',
+                                '-uiCochange', '2',
+                                '-crossingFanIn', '4',
+                                '-crossingFanOut', '4',
+                                '-cliqueDepends', 'call,use',
+                                '-uihDepends', 'call,use',
+                                '-uihInheritance', 'extend,implement,public,private,virtual',
+                                '-outputFolder', output_path,
+                                input_path),
+          stdout=FALSE, stderr=FALSE)
 }
