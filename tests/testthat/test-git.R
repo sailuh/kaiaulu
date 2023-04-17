2f54e51cf4c9287be333f70ef558232004cf4b0b
malialiu <mliu0141@gmail.com>
2023-04-16 16:13:37 -1000

    luh#154 add unit tests for git_checkout and parse_gitlog

--- tests/testthat/test-git.R
+++ tests/testthat/test-git.R
@@ -1,4 +1,4 @@
-test_that("test that calling git_checkout with correct branch name and an exist local path of github project returns list of string", {
+test_that("Calling git_checkout with correct branch name and an exist local path of github project returns list of string", {
   git_repo_path <- expect_warning(git_create_sample_log())
   branch_name <- ''
   result <- git_checkout(branch_name, git_repo_path)
@@ -7,14 +7,14 @@ test_that("test that calling git_checkout with correct branch name and an exist 
   expect_warning(git_delete_sample_log(git_repo_path))
 })
 
-test_that("test that calling git_checkout with incorrect branch name and an exist local path of github project returns warning", {
+test_that("Calling git_checkout with incorrect branch name and an exist local path of github project returns warning", {
   git_repo_path <- expect_warning(git_create_sample_log())
   branch_name <- "mas"
   expect_warning(git_checkout(branch_name, git_repo_path))
   expect_warning(git_delete_sample_log(git_repo_path))
 })
 
-test_that("test that calling git_checkout with correct branch name and non-exist local path of github project returns warning", {
+test_that("Calling git_checkout with correct branch name and non-exist local path of github project returns warning", {
   branch_name <- 'master'
   git_repo_path <- "~/Documents/some_random_path"
   expect_warning(git_checkout(branch_name, git_repo_path))
--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -1,4 +1,4 @@
-test_that("test that calling parse_gitlog with correct perceval and correct git log path returns a data table", {
+test_that("Calling parse_gitlog with correct perceval and correct git log path returns a data table", {
   tools_path <- "../../tools.yml"
   tool <- yaml::read_yaml(tools_path)
   perceval_path <- tool[["perceval"]]
@@ -8,7 +8,7 @@ test_that("test that calling parse_gitlog with correct perceval and correct git 
   expect_warning(git_delete_sample_log(git_repo_path))
 })
 
-test_that("test that calling parse_gitlog with incorrect perceval path and non-exist local github repository path returns error", {
+test_that("Calling parse_gitlog with incorrect perceval path and non-exist local github repository path returns error", {
   git_repo_path <- "~/Documents/some_random_project_path"
   perceval_path <- "/homeuser/"
   expect_error(expect_warning(parse_gitlog(perceval_path, git_repo_path)))
