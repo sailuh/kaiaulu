8679817fe1c8efddf3e734ca0cb4f61a3a49d68d
nicoelee123 <nicoelee123@gmail.com>
2023-04-24 00:51:52 -1000

    i #154 Fixed expect warnings in unit tests to suppress warnings
    Signed-off-by: nicoelee123 <nicoelee123@gmail.com>

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -2,10 +2,10 @@ test_that("Calling parse_gitlog with correct perceval and correct git log path r
   tools_path <- "../../tools.yml"
   tool <- yaml::read_yaml(tools_path)
   perceval_path <- tool[["perceval"]]
-  git_repo_path <- expect_warning(git_create_sample_log())
+  git_repo_path <- suppressWarnings(git_create_sample_log())
   result <- parse_gitlog(perceval_path, git_repo_path)
   expect_is(result, "data.table")
-  expect_warning(git_delete_sample_log(git_repo_path))
+  suppressWarnings(git_delete_sample_log(git_repo_path))
 })
 
 test_that("Calling parse_gitlog with incorrect perceval path and non-exist local github repository path returns error", {
