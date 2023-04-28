8bad33c198be335bc867d4164a683e42d2a88e94
nicoelee123 <nicoelee123@gmail.com>
2023-04-28 01:50:17 -1000

    Adding parse_gitlog test cases
    Signed-off-by: nicoelee123 <nicoelee123@gmail.com>

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -1,3 +1,9 @@
+test_that("Correct git repo path", {
+  git_repo_path <- suppressWarnings(git_create_sample_log())
+  expect_equal(file.exists(git_repo_path), TRUE)
+  suppressWarnings(git_delete_sample_log(git_repo_path))
+})
+
 test_that("Calling parse_gitlog with correct perceval and correct git log path returns a data table", {
   tools_path <- "../../tools.yml"
   tool <- yaml::read_yaml(tools_path)
@@ -8,8 +14,16 @@ test_that("Calling parse_gitlog with correct perceval and correct git log path r
   suppressWarnings(git_delete_sample_log(git_repo_path))
 })
 
-test_that("Calling parse_gitlog with incorrect perceval path and non-exist local github repository path returns error", {
-  git_repo_path <- "~/Documents/some_random_project_path"
-  perceval_path <- "/homeuser/"
-  expect_error(expect_warning(parse_gitlog(perceval_path, git_repo_path)))
+test_that("Calling parse_gitlog with incorrect perceval path returns correct error", {
+  git_repo_path <- suppressWarnings(git_create_sample_log())
+  incorrect_perceval_path <- "incorrect/path/to/perceval"
+  expect_error(parse_gitlog(perceval_path, git_repo_path), "object 'perceval_path' not found")
+})
+
+test_that("Calling parse_gitlog with incorrect git repo path returns correct error", {
+  tools_path <- "../../tools.yml"
+  tool <- yaml::read_yaml(tools_path)
+  perceval_path <- tool[["perceval"]]
+  incorrect_repo_path <- "incorrect/path/to/git_repo.git"
+  expect_error(parse_gitlog(perceval_path, incorrect_repo_path), "object 'data.Author' not found")
 })
