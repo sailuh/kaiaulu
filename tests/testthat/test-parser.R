0bdbb03b989ddf8a8e39097b1542b2dcbc4294a3
nicoelee123 <nicoelee123@gmail.com>
2023-04-29 15:17:32 -1000

    i #154 Making changes to unit tests
    Signed-off-by: nicoelee123 <nicoelee123@gmail.com>

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -1,27 +1,20 @@
-test_that("Using Perceval version 0.12.24", {
+test_that("Perceval version 0.12.24 is being used", {
   tools_path <- "../../tools.yml"
   tool <- yaml::read_yaml(tools_path)
   perceval_path <- tool[["perceval"]]
+  perceval_path <- path.expand(perceval_path)
   perceval_version <- system2(perceval_path, args="--version", stdout=TRUE, stderr=FALSE)
   expect_equal(perceval_version, "perceval 0.12.24")
 })
-test_that("Correct perceval path", {
+test_that("Perceval path in tools.yml is specified correctly", {
   tools_path <- "../../tools.yml"
   tool <- yaml::read_yaml(tools_path)
   perceval_path <- tool[["perceval"]]
   expect_equal(file.exists(perceval_path), TRUE)
 })
-test_that("Correct mbox path", {
-  conf_path <-"../../conf/thrift.yml"
-  conf <- yaml::read_yaml(conf_path)
-  mbox_path <- conf[["mailing_list"]][["mbox"]]
-  expect_equal(file.exists(mbox_path), TRUE)
-})
-test_that("Correct jira issues comments path", {
-  conf_path <-"../../conf/thrift.yml"
-  conf <- yaml::read_yaml(conf_path)
-  jira_issue_comments_path <- conf[["issue_tracker"]][["jira"]][["issue_comments"]]
-  expect_equal(file.exists(jira_issue_comments_path), TRUE)
+test_that("Configuration files are placed on recommended path", {
+  conf_path <- "../../conf/thrift.yml"
+  expect_equal(file.exists(conf_path), TRUE)
 })
 test_that("Incorrect perceval path fails parse_mbox", {
   conf_path <-"../../conf/thrift.yml"
@@ -40,5 +33,7 @@ test_that("Incorrect mbox path to parse_mbox returns empty table", {
 })
 test_that("Incorrect jira issue comments path fails parse_jira", {
   incorrect_jira_issue_comments_path <- "/incorrect/path/to/jira_issue_comments"
-  expect_error(parse_jira(incorrect_jira_issue_comments_path), "cannot open the connection")
+  suppressWarnings({
+    expect_error(parse_jira(incorrect_jira_issue_comments_path), "cannot open the connection")
+  })
 })
