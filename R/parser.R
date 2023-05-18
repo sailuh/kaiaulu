1511efd1fdfda6996dfe0e9b8d98eea90d3b28eb
Lihan Zhan <lzhan@sei.cmu.edu>
2023-05-18 09:09:40 -0400

    Added conf files for jhotdraw5.1, juni3.7, jrefacotry 2.6.24. Modified parser to take output_filepath from config file to avoid duplicate output file for different projects.

--- R/parser.R
+++ R/parser.R
@@ -1510,10 +1510,11 @@ parse_r_dependencies <- function(folder_path){
 #' IEEE Transactions on Software Engineering,
 #' vol. 32, no. 11, pp. 896-909, November, 2006.
 #' @export
-parse_gof_patterns <- function(pattern4_path,class_folder_path,output_filepath='/tmp/gof.xml'){
+parse_gof_patterns <- function(pattern4_path,class_folder_path,output_filepath){
 
   pattern4_path <- path.expand(pattern4_path)
   class_folder_path <- path.expand(class_folder_path)
+  output_filepath <- path.expand(output_filepath)
 
   if(!file.exists(pattern4_path)) stop("The specified pattern4_path does not exist!")
   if(!dir.exists(class_folder_path)) stop("The specified class_folder_path does not exist!")
