cc50f9f1c709a5013443951907e8d827e93556e4
Ruben <rjacobo@hawaii.edu>
2023-11-17 12:54:53 -1000

    congruent parameter names and timezone functionality

--- R/mail.R
+++ R/mail.R
@@ -8,55 +8,47 @@
 
 
 ## Creates fake mbox data
+#'
 #' @param mlist specific mailing list associated with this mbox data
-#' @param sender information of person sending the mail
-#' @param date First year in the range to be downloaded
-#' @param subject subject of the email
-#' @param content the body of the email
-#' @param randomise_email_case If TRUE, randomizes the email case. (Default = TRUE)
+#' @param reply_from email of sender
+#' @param reply_to email of receiver
+#' @param reply_cc carbon copy of email to additional recipients
+#' @param reply_datetime Date, time in the following format: 2023-02-11T09:30:00
+#' @param timezone Abbreviated timezone of the reply eg: HST or EST
+#' @param reply_subject subject of the email
+#' @param reply_body the body of the email
+#' @return the content of the fake mbox you created
 #' @export
-create_fake_mbox <- function(mlist, sender, date, subject, content, randomise_email_case = TRUE) {
+create_fake_mbox <- function(mlist, reply_from, reply_to, reply_cc, reply_datetime, timezone, reply_subject, reply_body) {
 
 # format the date correctly
-  cdate <- format(as.POSIXct(date, format = "%Y-%m-%dT%H:%M:%S"), "%a, %d %b %Y %H:%M:%S")
+ cdate <- format(as.POSIXct(reply_datetime, format = "%Y-%m-%dT%H:%M:%S "), "%a, %d %b %Y %H:%M:%S ")
 
-
-  #execute random concatenation of characters to replace the subject and content parameters if anonymity is desired
- # if (randomise_email_case) {
-  #  subject <- paste0(sample(c(tolower, toupper), nchar(subject), replace = TRUE),(strsplit(subject, "")), collapse = "")
- #   content <- paste0(sample(c(tolower, toupper), nchar(content), replace = TRUE),(strsplit(content, "")), collapse = "")
- # }
 
   mbox_content <- paste0(
-    "From MAILER-DAEMON Thu Jul 18 13:48:48 2013",
-    "\nPath: example.com!not-for-mail",
-    "\nFrom: ", sender,
-    "\nNewsgroups: gmane.", mlist,
-    "\nSubject: ", subject,
+    "From MAILER-DAEMON Thu Jul 18 13:48:48 2013 ",
+    "\nPath: example.com!not-for-mail ",
+    "\nFrom: ", reply_from,
+    "\nNewsgroups: gmane. ", mlist,
+    "\nSubject: ", reply_subject,
     "\nDate: ", cdate,
-    "\nApproved: auto",
-    "\nMessage-ID: <", length(content), "@example.com>",
+    "\nApproved: auto ",
+    "\nMessage-ID: <", length(reply_body), "@example.com> ",
     "\nNNTP-Posting-Host: example.com",
-    "\nMime-Version: 1.0",
-    "\nContent-Type: text/plain; charset=us-ascii; format=flowed",
-    "\nContent-Transfer-Encoding: 7bit",
-    "\nX-Complaints-To: complaints@example.com",
-    "\nNNTP-Posting-Date: ", cdate,
-    "\nUser-Agent: Mozilla/5.0 (X11; U; Linux i686; en-US; rv:0.9.8) Gecko/20020205",
-    "\nX-Accept-Language: en-us",
-    "\nOriginal-To: ", paste0(mlist, "@example.com"),
+    "\nMime-Version: 1.0 ",
+    "\nContent-Type: text/plain; charset=us-ascii; format=flowed ",
+    "\nContent-Transfer-Encoding: 7bit ",
+    "\nX-Complaints-To: complaints@example.com ",
+    "\nNNTP-Posting-Date: ", cdate, timezone,
+    "\nUser-Agent: Mozilla/5.0 (X11; U; Linux i686; en-US; rv:0.9.8) Gecko/20020205 ",
+    "\nX-Accept-Language: en-us ",
+    "\nOriginal-To: ", paste0(reply_to, "@example.com ", reply_cc, "@example.com "),
     "\nPrecedence: bulk",
-    "\nX-Mailing-List: ", paste0(mlist, "@example.com"),
-    "\n\n", content
+    "\nX-Mailing-List: ", paste0(mlist, "@example.com "),
+    "\n\n", reply_body
   )
 
   return(mbox_content)
 }
 
 
-# Call the function with specific values
-mlist <- "test-list"
-sender <- "john.doe@example.com"
-date <- "2023-01-15T08:30:00"
-subject <- "Test Email Subject"
-content <-  "This is the body of the test email."
