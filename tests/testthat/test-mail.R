9c6a67024fdc3b0d2e3d86b1be0e703b747ed6de
Ruben <rjacobo@hawaii.edu>
2023-11-17 15:34:56 -1000

    implement testing for example mbox parsing

---
+++ tests/testthat/test-mail.R
@@ -0,0 +1,13 @@
+tools_path <- test_path("testdata", "tools.yml")
+conf_path <- test_path("testdata", "thrift.yml")
+
+test_that("Calling parse_mbox with correct perceval and mbox path returns a data table with correct raw data", {
+  tools_path <- file.path(tools_path)
+  tool <- yaml::read_yaml(tools_path)
+  perceval_path <- tool[["perceval"]]
+  mbox_path <- example_mbox_normal <- function(folder_path = "/tmp", folder_name="sample_folder")  # Replace with the actual path to your mbox file
+  result <- parse_mbox(perceval_path, mbox_path)
+  expect_is(result, "data.table")
+
+})
+
