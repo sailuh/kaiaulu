51c2b4c802adf68e2e96752fbceaac53e75c6424
RavenMarQ <ravenmq@hawaii.edu>
2024-11-18 18:09:36 -1000

    Update understand.R
    From my understanding of the feedback received, updated the understand.R.

--- exec/understand.R
+++ exec/understand.R
@@ -1,6 +1,7 @@
 #!/usr/local/bin/Rscript
 
 require(kaiaulu, quietly = TRUE)
+require(cli, quietly = TRUE)
 require(XML, quietly = TRUE)
 require(stringi, quietly = TRUE)
 require(data.table, quietly = TRUE)
@@ -8,9 +9,12 @@ require(data.table, quietly = TRUE)
 # For building, should I create a separate executable? Or put it in as an option? Currently have it as a separate usage.
 doc <- "
 USAGE:
-  understand.R help
-  understand.R build <project_filepath> <code_language> <output_dir>
-  understand.R parse <build_filepath> [--class | --file]
+  understand.R (-h | -help)
+  understand.R --version
+  understand.R build help
+  understand.R build <config_filepath>
+  understand.R parse help
+  understand.R parse <config_filepath> <save_path> [--class | --file]
 
 DESCRIPTION:
   Builds then analyzes a project using Scitool's Understand for dependencies between either classes or files.
@@ -20,20 +24,15 @@ OPTIONS:
   --file    parses file-level dependencies
 "
 
-arguments <- docopt::docopt(doc, version = 'Kaiaulu 0.0.0.9600')
+arguments <- docopt::docopt(doc, version = 'Kaiaulu 0.0.0.9700')
 
 # Currently unsure how variables would work
 
-project_path <- ""
-understand_folder <- ""
-code_language <- ""
-
-if (arguments[["build"]]) {
-  project_path <- arguments[["<project_filepath>"]]
-  understand_folder <- arguments[["<output_dir>"]]
-  code_language <- arguments[["<code_language>"]]
-} else if (arguments[["parse"]]) {
-  understand_folder <- arguments[["<build_filepath>"]]
+if (!arguments[["help"]]) {
+  conf <- parse_config(arguments[["<config_filepath>"]])
+  project_path <- get_understand_project_path(conf)
+  understand_folder <- get_understand_output_path(conf)
+  code_language <- get_understand_code_language(conf)
 }
 
 # Ensuring directory exists for output
@@ -42,14 +41,25 @@ if (!dir.exists(understand_folder) & (arguments[["build"]] | arguments[["parse"]
 }
 
 # Determine which function to run and save output
-if (arguments[["build"]]) {
+if (arguments[["build"]] & arguments[["help"]]) {
+  cli::cli_alert_info("Builds an analysis of the project in the designated project_path set in the <config_filepath> using build_understand_project()")
+} else if (arguments[["parse"]] & arguments[["help"]]) {
+  cli::cli_alert_info("From the built analysis, parses the dependency types of either files or classes (dependening on supplied flag) using parse_understand_dependencies() and saves it at <save_path>")
+} else if (arguments[["build"]]) {
   build_understand_project(project_path = project_path, language = code_language, output_dir = understand_folder)
+  cli::cli_alert_success("Project sucessfully built.")
 } else if (arguments[["parse"]]) {
   if (arguments[["--file"]]) {
     result <- parse_understand_dependencies(understand_dir = understand_folder, parse_type = "file")
   } else if (arguments[["--class"]]) {
-    result <- parse_understand_dependencies(understand_dir = understand_folder, parse_type = "classs")
+    result <- parse_understand_dependencies(understand_dir = understand_folder, parse_type = "class")
   }
+  data.table::fwrite(result, save_path)
+  cli::cli_alert_success(paste0("Dependencies table was saved at: ", save_path))
+} else if (arguments[["-h"]] || arguments[["--help"]]) {
+  cli::cli_alert_info(doc)
+} else if (arguments[["--version"]]) {
+  cli::cli_alert_info('Kaiaulu 0.0.0.9700')
 } else {
   stop("No/invalid option(s) provided.")
 }
