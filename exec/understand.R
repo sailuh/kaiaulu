06b76e0861d849fc8c5a7cb7f28ce96fcfffee64
RavenMarQ <ravenmq@hawaii.edu>
2024-12-05 17:20:05 -1000

    Uploaded Completed executables
    Simple push after viewing during 12/5/2024 meeting

--- exec/understand.R
+++ exec/understand.R
@@ -6,7 +6,6 @@ require(XML, quietly = TRUE)
 require(stringi, quietly = TRUE)
 require(data.table, quietly = TRUE)
 
-# For building, should I create a separate executable? Or put it in as an option? Currently have it as a separate usage.
 doc <- "
 USAGE:
   understand.R (-h | -help)
@@ -33,6 +32,9 @@ if (!arguments[["help"]]) {
   project_path <- get_understand_project_path(conf)
   understand_folder <- get_understand_output_path(conf)
   code_language <- get_understand_code_language(conf)
+  save_path <- arguments[["<save_path>"]]
+  node_file <- paste0(save_path, "/node.csv")
+  edge_file <- paste0(save_path, "/edge.csv")
 }
 
 # Ensuring directory exists for output
@@ -44,7 +46,7 @@ if (!dir.exists(understand_folder) & (arguments[["build"]] | arguments[["parse"]
 if (arguments[["build"]] & arguments[["help"]]) {
   cli::cli_alert_info("Builds an analysis of the project in the designated project_path set in the <config_filepath> using build_understand_project()")
 } else if (arguments[["parse"]] & arguments[["help"]]) {
-  cli::cli_alert_info("From the built analysis, parses the dependency types of either files or classes (dependening on supplied flag) using parse_understand_dependencies() and saves it at <save_path>")
+  cli::cli_alert_info("From the built analysis, parses the dependency types of either files or classes (dependening on supplied flag) using parse_understand_dependencies() and saves two tables at the specified folder: <save_path>")
 } else if (arguments[["build"]]) {
   build_understand_project(project_path = project_path, language = code_language, output_dir = understand_folder)
   cli::cli_alert_success("Project sucessfully built.")
@@ -54,7 +56,8 @@ if (arguments[["build"]] & arguments[["help"]]) {
   } else if (arguments[["--class"]]) {
     result <- parse_understand_dependencies(understand_dir = understand_folder, parse_type = "class")
   }
-  data.table::fwrite(result, save_path)
+  data.table::fwrite(result$node_list, node_file)
+  data.table::fwrite(result$edge_list, edge_file)
   cli::cli_alert_success(paste0("Dependencies table was saved at: ", save_path))
 } else if (arguments[["-h"]] || arguments[["--help"]]) {
   cli::cli_alert_info(doc)
@@ -63,5 +66,3 @@ if (arguments[["build"]] & arguments[["help"]]) {
 } else {
   stop("No/invalid option(s) provided.")
 }
-
-# Unsure how to close up the executable at the moment, but result is a data.table
