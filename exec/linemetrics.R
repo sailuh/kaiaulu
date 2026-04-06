60f820a6e18e4eb672eeb240efae437fb1ed86d3
CohenRuport <cohenmruport@gmail.com>
2026-04-06 12:28:23 -1000

    Refactored src module execs
    Refactored execs from PR #329 for the understands, depends, kaiaulu_architecture, and line_metrics notebooks. Fixed issues with filters, save paths, readability, and consistency.

---
+++ exec/linemetrics.R
@@ -0,0 +1,62 @@
+#!/usr/local/bin/Rscript
+
+require(kaiaulu, quietly = TRUE)
+require(data.table, quietly = TRUE)
+require(magrittr, quietly = TRUE)
+require(knitr, quietly = TRUE)
+require(cli, quietly = TRUE)
+
+doc <- "
+USAGE:
+  linemetrics.R (-h | -help)
+  linemetrics.R --version
+  linemetrics.R parse help
+  linemetrics.R parse <tools.yml> <project_conf.yml> <save_path>
+
+DESCRIPTION:
+  Analyze line metrics using the SCC tool.
+
+OPTIONS:
+  -h --help     Show this screen.
+  --version     Show version.
+"
+
+arguments <- docopt::docopt(doc, version = 'Kaiaulu 0.0.0.9700')
+
+# Determine which function to run and save output
+if (arguments[["parse"]] & arguments[["help"]]) {
+  cli::cli_alert_info("Parses a project specified in the <project_conf.yml> config file and returns a csv file designated by <save_path> using SCC")
+} else if (arguments[["parse"]]) {
+  tool <- parse_config(arguments[["<tools.yml>"]])
+  conf <- parse_config(arguments[["<project_conf.yml>"]])
+  scc_path <- get_tool_project("scc", tool)
+
+  git_repo_path <- get_git_repo_path(conf)
+  git_branch <- get_git_branches(conf)[1]
+
+  # Filters
+  file_extensions <- get_file_extensions(conf)
+  substring_filepath <- get_substring_filepath(conf)
+
+  # Output path
+  save_path <- arguments[["<save_path>"]]
+
+  git_checkout(git_branch,git_repo_path)
+
+  # Parse Dependencies
+  result <- parse_line_metrics(scc_path,git_repo_path)
+
+  # Filter Parsed Dependencies
+  result <- result  %>%
+    filter_by_file_extension(file_extensions,"Provider")  %>%
+    filter_by_filepath_substring(substring_filepath,"Provider")
+
+  data.table::fwrite(result, save_path)
+  cli::cli_alert_success(paste0("Line metrics table was saved at: ", save_path))
+} else if (arguments[["-h"]] || arguments[["--help"]]) {
+  cli::cli_alert_info(doc)
+} else if (arguments[["--version"]]) {
+  cli::cli_alert_info('Kaiaulu 0.0.0.9700')
+} else {
+  stop("No/invalid option(s) provided.")
+}
---
+++ exec/understand.R
@@ -0,0 +1,110 @@
+#!/usr/local/bin/Rscript
+
+require(kaiaulu, quietly = TRUE)
+require(cli, quietly = TRUE)
+require(XML, quietly = TRUE)
+require(stringi, quietly = TRUE)
+require(data.table, quietly = TRUE)
+
+doc <- "
+USAGE:
+  understand.R (-h | -help)
+  understand.R --version
+  understand.R build help
+  understand.R build <tools.yml> <project_conf.yml>
+  understand.R export help
+  understand.R export <tools.yml> <project_conf.yml> <save_path> (--class | --file)
+  understand.R parse help
+  understand.R parse <tools.yml> <project_conf.yml> <node_save_file_name_path> <edge_save_file_name_path> (--class | --file)
+
+DESCRIPTION:
+  Builds then analyzes a project using Scitool's Understand for dependencies between either classes or files.
+
+OPTIONS:
+  -h --help     Show this screen.
+  --version     Show version.
+  --class       parses class-level dependencies
+  --file        parses file-level dependencies
+"
+
+arguments <- docopt::docopt(doc, version = 'Kaiaulu 0.0.0.9700')
+
+if (!arguments[["help"]]) {
+  tool <- parse_config(arguments[["<tools.yml>"]])
+  scitools_path <- get_tool_project("scitools", tool)
+
+  conf <- parse_config(arguments[["<project_conf.yml>"]])
+  keep_dependencies_type <- get_understand_keep_dependencies_type(conf)
+  project_path <- get_understand_project_path(conf)
+
+  # Scitools
+  understand_folder <- get_understand_output_path(conf)
+  code_language <- get_understand_code_language(conf)
+
+  db_path <- stringi::stri_c(understand_folder,"Understand.und")
+
+  file_dependencies_path <- stringi::stri_c(understand_folder,"file_dependencies.xml")
+  class_dependencies_path <- stringi::stri_c(understand_folder,"class_dependencies.xml")
+
+  save_path <- arguments[["<save_path>"]]
+
+  node_save_path <- arguments[["<node_save_file_name_path>"]]
+  edge_save_path <- arguments[["<edge_save_file_name_path>"]]
+
+
+}
+
+# Determine which function to run and save output
+if (arguments[["build"]] & arguments[["help"]]) {
+  cli::cli_alert_info("Builds an analysis of the project in the designated project_path set in the <config_filepath.yml> using build_understand_project()")
+} else if (arguments[["export"]] & arguments[["help"]]) {
+  cli::cli_alert_info("From the built analysis, exports the dependency types of either files or classes (dependening on supplied flag) using export_understand_dependencies() and saves two xmls at the specified file paths: <node_save_file_name_path> <edge_save_file_name_path>")
+} else if (arguments[["parse"]] & arguments[["help"]]) {
+  cli::cli_alert_info("From the built analysis, parses the dependency types of either files or classes (dependening on supplied flag) using parse_understand_dependencies() and saves two csvs at the specified file paths: <node_save_file_name_path> <edge_save_file_name_path>")
+} else if (arguments[["build"]]) {
+  db_path <- build_understand_project(scitools_path = scitools_path,
+                                      project_path = project_path,
+                                      language = code_language,
+                                      output_dir = understand_folder)
+  cli::cli_alert_success("Project sucessfully built.")
+} else if (arguments[["export"]]) {
+  if (arguments[["--file"]]) {
+    result <- export_understand_dependencies(scitools_path = scitools_path,
+                                             db_filepath = db_path,
+                                             parse_type = "file",
+                                             output_filepath = save_path)
+  } else if (arguments[["--class"]]) {
+    result <- export_understand_dependencies(scitools_path = scitools_path,
+                                             db_filepath = db_path,
+                                             parse_type = "class",
+                                             output_filepath = save_path)
+  } else {
+    stop("No/invalid option(s) provided.")
+  }
+  cli::cli_alert_success(paste0("Dependencies xml was saved at: ", save_path))
+} else if (arguments[["parse"]]) {
+  if (arguments[["--file"]]) {
+    result <- parse_understand_dependencies(dependencies_path = file_dependencies_path)
+
+    data.table::fwrite(result$node_list, node_save_path)
+    data.table::fwrite(result$edge_list, edge_save_path)
+
+  } else if (arguments[["--class"]]) {
+    result <- parse_understand_dependencies(dependencies_path = class_dependencies_path)
+
+    data.table::fwrite(result$node_list, node_save_path)
+    data.table::fwrite(result$edge_list, edge_save_path)
+  } else {
+    stop("No/invalid option(s) provided.")
+  }
+
+  cli::cli_alert_success(paste0("Dependencies node table was saved at: ", node_save_path))
+  cli::cli_alert_success(paste0("Dependencies edge table was saved at: ", edge_save_path))
+
+} else if (arguments[["-h"]] || arguments[["--help"]]) {
+  cli::cli_alert_info(doc)
+} else if (arguments[["--version"]]) {
+  cli::cli_alert_info('Kaiaulu 0.0.0.9700')
+} else {
+  stop("No/invalid option(s) provided.")
+}
